09800cb57833e57202ddec7dbb7ace12edac6a7a
Rishabh mishra <106048511+Rogger0071@users.noreply.github.com>
2025-08-04 20:30:02 +0530

    num2.py

---
+++ num2.py
@@ -0,0 +1,6 @@
+# NumPy is a Python library and is written partially in Python, but most of the parts that require fast computation are written in C or C++.
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr)
