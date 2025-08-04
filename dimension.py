d514765e5baa2f844af9e84dece9d4b931f62338
Rishabh mishra <106048511+Rogger0071@users.noreply.github.com>
2025-08-04 20:38:19 +0530

    dimension.py

---
+++ dimension.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
